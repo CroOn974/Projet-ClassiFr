51dd9f63da96bdce509120c99f5a92aa48a5e0ee
Vacarme Guillaume <vacarme.guillaume@gmail.com>
2023-02-22 10:49:29 +0400

    add vue js

---
+++ backend/api/views.py
@@ -0,0 +1,37 @@
+from api.models import Model, DateCreate
+from api.serializers import ModelSerializer, DateSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+class AdminModelViewset(viewsets.ModelViewSet):
+    
+    serializer_class = ModelSerializer
+
+    def get_queryset(self):
+        # retourne tous les modèles
+        queryset = Model.objects.all()
+
+        # retourne les modèle selon le status
+        status = self.request.GET.get('status')
+        if status is not None:
+            queryset = Model.objects.filter(status = status)
+        return queryset
+
+    # permet de changé le status d'un modèle
+    @action(detail=True, methods=['POST'])
+    def etatStatus(self, request, pk):
+        model = self.get_object()
+        model.etatStatus()
+        return Response({'status': model.status})
+
+
+class ModelViewset(viewsets.ReadOnlyModelViewSet):
+    queryset = Model.objects.filter(status = True)
+    serializer_class = ModelSerializer
+
+ 
+class DateViewset(viewsets.ModelViewSet):
+    queryset = DateCreate.objects.all()
+    serializer_class = DateSerializer
---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      models: []
+    }
+  },
+  async created(){
+    var response = await fetch('http://localhost:8000/api/admin/model');
+    this.models = await response.json();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
