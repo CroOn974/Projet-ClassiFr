22a1ba7d569e8e51946d187b650ce89c7b8128d3
DevIA.E14 <DevIA.E14@afpar.com>
2023-02-27 13:56:37 +0400

    LE FRONT

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -45,19 +45,19 @@
                         <td class="whitespace-nowrap px-6 py-4">
                         <div class="flex items-center space-x-4">
                                 
-                                <!-- BOUTOUN UPDATE -->
+                                <!-- BOUTON UPDATE -->
 
-                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="updateModel(model.id_model)">
                                 Edit
                                 </button>
 
-                                <!-- BOUTOUN DELETE -->
+                                <!-- BOUTON DELETE -->
 
                                 <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" @click="deleteModel(model.id_model)">
                                 Delete
                                 </button>
 
-                                <!-- BOUTOUN AFFICHAGE -->
+                                <!-- BOUTON AFFICHAGE -->
 
                                 <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" @click="toggleChildRow(model.id_model)" >
                                 Show details
@@ -86,7 +86,68 @@
         </div>
     </div>
 
+    <div class="hidden flex items-center justify-center" id="UpdateModal">
+      <div
+        class="fixed z-10 inset-0 overflow-y-auto"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div
+            class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            aria-hidden="true"
+          ></div>
+          <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+          <div
+            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modal-title"
+          >
+            <div>
+              <div class="mt-3 text-center sm:mt-5">
+                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                  Modifier un modèle
+                </h3>
+                <div class="mt-2">
+                  <p class="text-sm text-gray-500">
+                    Veuillez renseigner toutes les informations nécessaire pour l'utilisation du modèle
+                  </p>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-6">
+                <form class="flex flex-col">
+                  <label for="model_name" class="my-3">Nom</label>
+                  <input v-model="model.name" type="text" name="Nom du modèle" id="model_name" class="rounded-md border border-gray-300">
+                  <label for="model_acc" class="my-3">Accuracy</label>
+                  <input v-model="model.accuracy" type="int" name="Accuracy du modèle" id="model_acc" class="rounded-md border border-gray-300">
+                  <label for="model_info" class="my-3">Info</label>
+                  <input v-model="model.info" type="text" name="Info du modèle" id="model_info" class="rounded-md border border-gray-300">
+                </form>
+              </div>
+            </div>
+            <div>
+              <button
+                type="button"
+                class="inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 text-base font-medium text-gray-700 m-3"
+                @click="updateModel"
+              >
+                Modifier
+              </button>
 
+              <button
+                type="button"
+                class="inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700"
+                @click="updateModel"
+              >
+                Annuler
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     
 
 </template>
@@ -156,7 +217,11 @@ export default {
                 break;
                 }
             }
-            }
+        },
+        updateModel(id){
+            console.log(id)
+            document.getElementById('UpdateModal').classList.toggle("hidden")
+        }
     }
 };
 </script>
--- frontend/src/views/PredView.vue
+++ frontend/src/views/PredView.vue
@@ -1,12 +1,78 @@
 <template>
-  <div class="about">
-    <p>THIS IS PREDICTION PAGE</p>
+
+  <!-- IMAGES FORM  -->
+  <div class="w-96 rounded-lg bg-gray-800 text-slate-50 p-8 mx-auto my-8">
+    <form @submit.prevent="submitImage">
+      <label for="image">Sélectionner des images :</label>
+      <input type="file" id="image" ref="image" accept="image/*" multiple required class="my-4">
+      <button type="submit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded my-4 mx-24">Soumettre</button>
+    </form>
+  </div>
+
+  <!-- Affichage images soumises -->
+  <div v-if="imageDataList.length > 0" class="w-10/12 bg-gray-800 m-auto p-12 my-4 rounded-2xl">
+      <div class="m-4">
+          <h3 class="font-bold text-lg text-slate-50 w-12/12">Récapitulatif des images soumises :</h3>
+      </div>
+      <div class="flex flex-wrap">
+          <div v-for="(imageData, index) in imageDataList" :key="index" class="m-4">
+              <div class="block w-96 rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+                  <a href="#!">
+                      <img :src="imageData" alt="Image soumise par l'utilisateur" class="rounded-t-lg h-64 object-cover w-full">
+                  </a>
+                  <div class="p-6">
+                      <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50"> 
+                          Label
+                      </h5>
+                      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                          Taux de confiance
+                      </p>
+                      <button type="button" class="inline-block rounded bg-red-500 px-6 pt-2.5 pb-2 mr-4 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-red-600" data-te-ripple-init data-te-ripple-color="light">
+                          KO
+                      </button>
+                      <button type="button" class="inline-block rounded bg-green-500 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-green-600" data-te-ripple-init data-te-ripple-color="light">
+                          OK
+                      </button>
+                  </div>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
+
 <script>
-
 export default {
-  name: 'PredView',
-}
+  name:'PredView',
+  data() {
+    return {
+      imageDataList: []
+    }
+  },
+  methods: {
+    async submitImage() {
+      const files = this.$refs.image.files;
 
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+
+        // Vérifier si le fichier est une image
+        if (!file.type.startsWith("image/")) {
+          console.error(`Le fichier ${file.name} n'est pas une image.`);
+          continue;
+        }
+
+        try {
+          // Lire le contenu du fichier et le stocker dans la variable imageDataList
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+            this.imageDataList.push(reader.result);
+          };
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  }
+}
 </script>
